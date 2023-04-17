d52219383428e7c3ce315013c698ab4027af62c2
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-04-17 21:36:53 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+// Funkcja generująca losowy kolor w formacie heksadecymelowym z zadania
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Funkcja zmieniająca kolor
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+// Działanie Start raz na sekunde zmienia kolor tło,
+// Start zostaje zablokowany poprzez ustawienie atrybutu disabled na true
+function changerColor() {
+  changeEl = setInterval(changeColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+// Działanie Stop
+function changerColorStop() {
+  clearInterval(changeEl);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+// Zdarzenia start i stop
+startBtn.addEventListener('click', changerColor);
+stopBtn.addEventListener('click', changerColorStop);
