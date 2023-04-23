fc3134b579079487def13e293cc52557a0d58c44
Aleksandra8485 <118071817+Aleksandra8485@users.noreply.github.com>
2023-04-23 19:14:34 +0000

    Deploying to gh-pages from @ Aleksandra8485/goit-js-hw-09@27ac5dd6aba7c4c3790065e0bf337be2aa40c8a7 🚀

--- 01-color-switcher.9dba32b5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){changeEl=setInterval(n,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(changeEl),e.disabled=!1,t.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.9dba32b5.js.map
